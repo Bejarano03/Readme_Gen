1d5edb4150fae86a08b90a03f921a382c3e4b1d9
unknown <marco.bejarano03@gmail.com>
2020-12-03 12:23:19 -0800

    added requirements and create file async const

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = [
